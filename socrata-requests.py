0c493362cd4e0401d91d14fc820d511087b98023
Trent Robbins <robbintt@gmail.com>
2014-06-06 13:03:17 -0700

    Socrata isn't quite working.

--- socrata-requests.py
+++ socrata-requests.py
@@ -5,11 +5,16 @@ import requests
 Follow the pattern in 'auth' below when building the get request.  
 That means a dict with 2 key-value pairs, one each for username and password.
 """
+
+empty_credentials = { "username" : "", "password" : "" }
+
 with open('oauth.auth', 'r') as f:
     credentials = json.load(f)
 
+#credentials = empty_credentials
+
 print credentials
-r = requests.get('https://api.github.com/user', auth=(credentials['username'], credentials['password']))
+r = requests.get('https://data.sfgov.org/resource/g8m3-pdis.json', X-App-Token=(credentials['username']) )
 
 print r.status_code
 print r.headers['content-type']
