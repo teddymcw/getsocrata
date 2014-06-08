a7f3bcbc199ac8142451d0683f64297f47cc2f21
Trent Robbins <robbintt@gmail.com>
2014-06-07 17:22:25 -0700

    Built a skeleton paging generator in BASH, will move logic to python.

--- retrieve.sh
+++ retrieve.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-url="https://data.sfgov.org/resource/g8m3-pdis.json"
-outfile="out.dat"
+# record limit on sfgov.org (socrata) is 1000 records
+#url="https://data.sfgov.org/resource/g8m3-pdis.json"
 
-# store authentication as a string in a .auth file
+offset=0
+limit=1000
+
+# do this wherever the script is run
+directory='dat'
+mkdir -p $directory
+
+# authentication stored as $auth="<token>" in this source file (*.auth files are in .gitignore)
 source ./sh_auth.auth
 
-python getsocrata.py --url $url --outfile $outfile --auth $auth
+while true # until we get the right response
+do
+
+    url="https://data.sfgov.org/resource/g8m3-pdis.json?\$limit="$limit"&\$offset="$offset
+    outfile=$directory"/out"$offset"."`expr $offset + $limit`".dat"
+    
+    python getsocrata.py --url $url --outfile $outfile --auth $auth
+    
+    offset=`expr $offset + $limit`
+    echo $offset
+
+    sleep 2
+
+done # stop snagging data
